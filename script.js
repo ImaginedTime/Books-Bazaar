743f1f17f2019944b86241a11f45bbdb4af9afae
Uday <udayomsrivastava111@gmail.com>
2023-03-07 16:15:47 +0530

    Hamburger menu convert to a cross on click Added the add button to each card

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
     $(".hamburger").click(function () {
         $(".nav-links").slideToggle();
+        $(".line").toggleClass("active");
+
+    });
+
+    // selecting the add button
+    $(".book-card a:not([href='#'])").click(function () {
+        let content = this.innerHTML;
+        if (content == "Add +") {
+            this.innerHTML = "Added";
+            this.style.backgroundColor = "#4f4";
+        }
+        else {
+            this.innerHTML = "Add +";
+            this.style.backgroundColor = "#333";
+        }
     });
 });
 
