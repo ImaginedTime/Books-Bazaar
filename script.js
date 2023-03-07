f9c77873790d4ddf59e42af970f031c5b5ae95a9
Uday <udayomsrivastava111@gmail.com>
2023-03-07 17:01:52 +0530

    figured out the number animation for the about us section.

--- script.js
+++ script.js
@@ -19,6 +19,44 @@ $(document).ready(function () {
     });
 });
 
+// to animate the numbers animation
+const interval = setInterval(check_if_in_view, 50);
+
+function check_if_in_view() 
+{
+    var window_height = $(window).height();
+    var window_top_position = $(window).scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    var elem = $(".animate:last-child");
+    var elem_height = elem.outerHeight();
+    var elem_top_position = elem.offset().top;
+    var elem_bottom_position = (elem_top_position + elem_height);
+
+    if ((elem_bottom_position >= window_top_position) && (elem_top_position <= window_bottom_position)) 
+    {
+        clearInterval(interval);
+        $.each($(".animate"), function () {
+            animateValue(this, 0, this.innerHTML, 5000);
+        });
+    }
+}
+
+function animateValue(obj, start, end, duration) 
+{
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) 
+            startTimestamp = timestamp;
+
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) 
+            window.requestAnimationFrame(step);
+    };
+    window.requestAnimationFrame(step);
+}
+
 // // to check whether the media query detects properly
 // setInterval(check, 50);
 // function check(){
